2b8f06389f20031c39132e04b82638786ba8d71f
CV <1140222138@qq.com>
2022-06-16 14:17:39 +0930

    3

--- public/javascripts/MY_VUE.js
+++ public/javascripts/MY_VUE.js
@@ -2,11 +2,33 @@ let app = new Vue({
     el: '#main',
     data:
         {
-            time:0,
-            seating_section:0
+            time: 0,
+            seating_section: "DEFAULT",
+            seat_list: [
+                {date: '2022-6-10', seat_ID: 1, price: 100, seating_section: 'Privilege Seat'}]
         },
     methods:
         {
+            check()
+            {
+                let that = this;
 
-        }
+                MY_AJAX({
+                    type: 'POST',
+                    url: '/get_seat',
+                    headers:
+                        {
+                            'Content-Type': 'application/json'
+                        },
+                    success: function (response)
+                    {
+                        that.seat_list = response
+                    },
+                    error: function (response)
+                    {
+                        console.log('get seat list failed')
+                    }
+                })
+            }
+        },
 })
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/get_seat', function(req, res, next)
+{
+  let body = req.body;
+  console.log(body);
+
+  req.pool.getConnection(function (err, connection)
+  {
+    if (err)
+    {
+      res.sendStatus(500)
+      return console.log(err.message)
+    }
+    res.send()
+  })
+})
+
 module.exports = router;
